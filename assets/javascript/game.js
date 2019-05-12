6f14efb247de076766bed9d6e518897b1efda817
Rachel Givens <rachelgivens@me.com>
2019-05-11 21:00:44 -0500

    Updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,64 @@
 // We start by creating an array of words
-var words = [
-    "javascript",
-    "html",
-    "css",
-    "bootstrap",
-    "vanderbilt",
-    "bootcamp"
-];
+var words= ["addams", "morticia", "wednesday", "uncle_fester", "lurch", "pugsley", "thing", "cousin_itt", "grandmama"];
+
+var display = [wordSelection];
+var win = wordSelection;
+var output ="";
+var userLetters="";
 
 // This will generate a random word from the length of array 
-var word = words[Math.floor(Math.random()*words.length)]
+var wordSelection = words[Math.floor(Math.random() * words.length)];
 
-// This will loop around creating the empty spaces for the word therefore setting up the asnwer array
-var answerArray = [];
-for (var i=0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-var remaingLetters = word.length;
-
-// This is the game loop 
-while (remainingLetters > 0) {
-// She player the progress 
-    alert(answerArray.join("  "));
-// User enters a guess
-var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    if (guess === null) {
-    // user exits the game loop
-        break;
-} else if (guess.length !=== 1) {
-    alert("Please enter a single letter.");
-} else {
-    // This is where the game gets updated with the new stats
-// We are creating a new loop for a new variable which is h
-    for(var h=0; h < word.length; h++) {  
-// this is representing the letter we are looking at matches the players guess
-if (word[h] === guess) {
-// if it does then we use this to update the asnwer array with the current guess
-    answerArray[h] = guess; 
-    remainingLetters--;
-        }
+var setup = function () {
+    for (var i=0; i< words.length; i++)
+    {
+        display[i] = "_";
+        output = output + display[i];
     }
-}
+    document.getElementbyId("hangman").inneHTML = output;
+    output ="";
+
 }
 
-// This is the message that lets the user know they won
-alert(answerArray.join(" "));
-alert("CONGRATS!!! The answer was " + word);
+var submit = function() {
+    output = "";
+    uderLetter = ("letter").value;
+    ("letter").value;
+
+    for (var l=0; l< words.length; l++)
+    {
+        display[l] = userLetter.toLowerCase();
+        win--;
+    }
+    output = output + display[l] + " ";
+}
+document.getElementbyId("hangman").innerHTML = output;
+output="";
+attemptsLeft--;
+
+if (win < 1)
+{
+    document.getElementbyId("guesses").innerHTML = "Congrats you win!!";
+}
+else if (attempts < 1)
+{
+    document.getElementbyId("guesses").innerHTML = "Sorry try again";
+}
+else
+{
+    document.getElementbyId("guesses").innerHTML = "You have "
++ attemptsLeft + "guesses left...";
+}
+
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+              }
+
+
+
+
+
 
 
 
